110958584bda6a0e1581be472cb60659f39da6e3
Tamas Galffy <ezittgtx@gmail.com>
2021-11-02 17:03:48 +0100

    fix: Update NetworkUIActions
    The implementation only supported one kind of transport for setting
    connection data. Updated to support both ENet and UnityTransport, or not
    set any data in case of unkonwn transport.

--- Assets/Scripts/NetworkUIActions.cs
+++ Assets/Scripts/NetworkUIActions.cs
@@ -10,10 +10,24 @@ public class NetworkUIActions : MonoBehaviour
 
     public void Connect()
     {
-        var networkManager = NetworkManager.Singleton;
-        var networkTransport = networkManager.GetComponent<EnetTransport>();
+        string address = addressInput.text.Trim();
+        address = address.Length > 0 ? address : "127.0.0.1";
 
-        networkTransport.Address = addressInput.text;
+        ushort port = 7777;
+
+        var networkManager = NetworkManager.Singleton;
+        
+        var enetTransport = networkManager.GetComponent<EnetTransport>();
+        var unityTransport = networkManager.GetComponent<UnityTransport>();
+
+        if (enetTransport)
+        {
+            enetTransport.Address = address;
+            enetTransport.Port = port;
+        }
+        else if (unityTransport)
+            unityTransport.SetConnectionData(Unity.Networking.Transport.NetworkEndPoint.Parse(address, port));
+
         networkManager.StartClient();
     }
 
